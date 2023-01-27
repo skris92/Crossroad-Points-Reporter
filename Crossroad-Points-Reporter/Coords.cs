12ad59dbcd9ee45f36536d2a30f45cf56f47cfa1
skris92 <szurcsik92@gmail.com>
2023-01-27 14:12:57 +0100

    Coords class

---
+++ Crossroad-Points-Reporter/Coords.cs
@@ -0,0 +1,19 @@
+﻿namespace Crossroad_Points_Reporter
+{
+    public class Coords
+    {
+        public int X { get; private set; }
+
+        public int Y { get; private set; }
+
+        public void SetX(int coordinateX)
+        {
+            X = coordinateX;
+        }
+
+        public void SetY(int coordinateY)
+        {
+            Y = coordinateY;
+        }
+    }
+}
