2db06b31b629307e1cd27c25f326746fdfe5fecb
skris92 <szurcsik92@gmail.com>
2023-01-28 17:28:23 +0100

    CalculateAreaSize method

---
+++ Crossroad-Points-Reporter/DiagramCreator.cs
@@ -0,0 +1,38 @@
+﻿using InputFileParserClassLibrary;
+
+namespace Crossroad_Points_Reporter
+{
+    public class DiagramCreator
+    {
+        private int[][] _area;
+        private List<int[]> _crossroadPoints = new();
+
+        public int[][] CreateDiagram(List<VentLine> ventLines)
+        {
+            _area = new int[3][];
+            return _area;
+        }
+
+        private Dictionary<string, int> CalculateAreaSize(List<VentLine> ventLines)
+        {
+            Dictionary<string, int> areaSize = new Dictionary<string, int>();
+
+            int maxWidth = 0;
+            int maxHeight = 0;
+
+            foreach (VentLine line in ventLines)
+            {
+                if (maxWidth < line.StartCoords.X) maxWidth = line.StartCoords.X;
+                if (maxWidth < line.EndCoords.X) maxWidth = line.EndCoords.X;
+
+                if (maxHeight < line.StartCoords.Y) maxHeight = line.StartCoords.Y;
+                if (maxHeight < line.EndCoords.Y) maxHeight = line.EndCoords.Y;
+            }
+
+            areaSize.Add("maxWidth", maxWidth);
+            areaSize.Add("maxHeight", maxHeight);
+
+            return areaSize;
+        }
+    }
+}
