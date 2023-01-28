31b1c421ae5c3a7b4ef563af29191b25fbfcdc50
skris92 <szurcsik92@gmail.com>
2023-01-28 20:18:38 +0100

    DrawVentLine, SetVentPoint methods

--- Crossroad-Points-Reporter/DiagramCreator.cs
+++ Crossroad-Points-Reporter/DiagramCreator.cs
@@ -5,11 +5,16 @@ namespace Crossroad_Points_Reporter
     public class DiagramCreator
     {
         private int[,] _area;
-        private Dictionary<int[,], int> _crossroadPoints = new();
+        private Dictionary<string, int> _crossroadPoints = new();
 
         public int[,] CreateDiagram(List<VentLine> ventLines)
         {
             InitializeDiagram(ventLines);
+
+            foreach (var line in ventLines)
+            {
+                DrawVentLine(line);
+            }
 
             return _area;
         }
@@ -30,8 +35,8 @@ namespace Crossroad_Points_Reporter
                 if (maxHeight < line.EndCoords.Y) maxHeight = line.EndCoords.Y;
             }
 
-            areaSize.Add("Width", maxWidth);
-            areaSize.Add("Height", maxHeight);
+            areaSize.Add("Width", maxWidth + 1);   // + 1 because diagram coordinates starts from 0
+            areaSize.Add("Height", maxHeight + 1);
 
             return areaSize;
         }
@@ -41,6 +46,54 @@ namespace Crossroad_Points_Reporter
             Dictionary<string, int> area = CalculateAreaSize(ventLines);
 
             _area = new int[area["Height"], area["Width"]];
+        }
+
+        private void DrawVentLine(VentLine ventLine)
+        {
+            // vertical line
+            if (ventLine.StartCoords.X == ventLine.EndCoords.X &&
+                ventLine.StartCoords.Y < ventLine.EndCoords.Y)
+            {
+                for (int i = ventLine.StartCoords.Y; i <= ventLine.EndCoords.Y; i++)
+                {
+                    SetVentPoint(ventLine.StartCoords.X, i);
+                }
+            }
+            else if (ventLine.StartCoords.X == ventLine.EndCoords.X && 
+                     ventLine.StartCoords.Y > ventLine.EndCoords.Y)
+            {
+                for (int i = ventLine.StartCoords.Y; i >= ventLine.EndCoords.Y; i--)
+                {
+                    SetVentPoint(ventLine.StartCoords.X, i);
+                }
+            }
+
+            // horizontal line
+            if (ventLine.StartCoords.Y == ventLine.EndCoords.Y &&
+                ventLine.StartCoords.X < ventLine.EndCoords.X)
+            {
+                for (int i = ventLine.StartCoords.X; i <= ventLine.EndCoords.X; i++)
+                {
+                    SetVentPoint(i, ventLine.StartCoords.Y);
+                }
+            }
+            else if (ventLine.StartCoords.Y == ventLine.EndCoords.Y &&
+                     ventLine.StartCoords.X > ventLine.EndCoords.X)
+            {
+                for (int i = ventLine.StartCoords.X; i >= ventLine.EndCoords.X; i--)
+                {
+                    SetVentPoint(i, ventLine.StartCoords.Y);
+                }
+            }
+        }
+
+        private void SetVentPoint(int coordX, int coordY)
+        {
+            _area[coordY, coordX]++;
+            if (_area[coordY, coordX] > 1)
+            {
+                _crossroadPoints.Add($"({coordX}, {coordY})", _area[coordY, coordX]);
+            }
         }
     }
 }
