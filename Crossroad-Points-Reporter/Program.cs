7d704ee1e6691a3de0e4178843b8aa71cbd1bed5
skris92 <szurcsik92@gmail.com>
2023-01-28 23:58:07 +0100

    Program code

--- Crossroad-Points-Reporter/Program.cs
+++ Crossroad-Points-Reporter/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using InputFileParserClassLibrary;
+using Crossroad_Points_Reporter;
+
+List<VentLine> ventLines = InputFileParser.GetVentLines();
+
+Diagram diagram = DiagramCreator.CreateDiagram(ventLines);
+
+diagram.Display();
+
+diagram.DisplayCrossroadPoints();
