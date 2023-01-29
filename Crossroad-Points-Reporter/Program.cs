04120fb05db780356f413790db85c0dfc3855a86
skris92 <szurcsik92@gmail.com>
2023-01-29 22:32:52 +0100

    Cooments added

--- Crossroad-Points-Reporter/Program.cs
+++ Crossroad-Points-Reporter/Program.cs
@@ -2,16 +2,20 @@
 using Crossroad_Points_Reporter;
 using InputFileParserDLL;
 
+// Validating input format and converting data to VentLine objects
 List<VentLine> ventLines = InputFileParser.GetVentLines();
 
+// Creating Diagram object from VentLine objects
 Diagram diagram = DiagramCreator.CreateDiagram(ventLines);
 
+// Visualizing diagram
 Display.Diagram(diagram.Area);
 
+// Visualizing result
 string result = diagram.GetCrossroadPointsReport();
-
 Display.Result(result);
 
+// Exporting result
 if (ResultExporter.UserWantsToExport())
 {
     ResultExporter.Export(InputFileParser.DirectoryPath, result);
