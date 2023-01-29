8ab5dd48e4af0321b25fe67c6c7b3f548d333cea
skris92 <szurcsik92@gmail.com>
2023-01-30 00:13:16 +0100

    Program only quits on user input "Q" or "q"

--- Crossroad-Points-Reporter/Program.cs
+++ Crossroad-Points-Reporter/Program.cs
@@ -2,21 +2,26 @@
 using Crossroad_Points_Reporter;
 using InputFileParserDLL;
 
-// Validating input format and converting data to VentLine objects
-List<VentLine> ventLines = InputFileParser.GetVentLines();
-
-// Creating Diagram object from VentLine objects
-Diagram diagram = DiagramCreator.CreateDiagram(ventLines);
-
-// Visualizing diagram
-//Display.Diagram(diagram.Area); -- I misunderstood the task, it wasn't a requirement
-
-// Visualizing result
-string result = diagram.GetCrossroadPointsReport();
-Display.Result(result);
-
-// Exporting result
-if (ResultExporter.UserWantsToExport())
+// Program runs until any of the user inputs is "Q" or "q"
+while (true)
 {
-    ResultExporter.Export(InputFileParser.DirectoryPath, result);
+    // Validating input format and converting data to VentLine objects
+    List<VentLine> ventLines = InputFileParser.GetVentLines();
+
+    // Creating Diagram object from VentLine objects
+    Diagram diagram = DiagramCreator.CreateDiagram(ventLines);
+
+    // Visualizing diagram
+    //Display.Diagram(diagram.Area); -- I misunderstood the task, it wasn't a requirement
+
+    // Visualizing result
+    string result = diagram.GetCrossroadPointsReport();
+    Display.Result(result);
+
+    // Exporting result
+    if (ResultExporter.UserWantsToExport())
+    {
+        ResultExporter.Export(InputFileParser.DirectoryPath, result);
+
+    }
 }
