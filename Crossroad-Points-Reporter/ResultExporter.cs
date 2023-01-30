fb2771a02bc0a5c96002a8e2a69965c3ebca25ec
skris92 <szurcsik92@gmail.com>
2023-01-30 14:03:48 +0100

    Feature: Override check on existing file when exporting.

--- Crossroad-Points-Reporter/ResultExporter.cs
+++ Crossroad-Points-Reporter/ResultExporter.cs
@@ -19,8 +19,47 @@
 
         public static void Export(string directoryPath, string result)
         {
-            string fileName = directoryPath + "\\" + GetFileName();
+            bool askingFileName = true;
 
+            while (askingFileName)
+            {
+                string fileName = directoryPath + "\\" + GetFileName();
+
+                if (File.Exists(fileName))
+                {
+                    if (UserWantsToOverride())
+                    {
+                        WriteTextToFile(fileName, result);
+                        askingFileName = false;
+                    }
+                }
+                else
+                {
+                    WriteTextToFile(fileName, result);
+                    askingFileName = false;
+                }
+            }
+        }
+
+        private static bool UserWantsToOverride()
+        {
+            // Asking for user input until option yes, no or quit selected
+            string? chosenOption = "";
+
+            while (chosenOption != "Y" && chosenOption != "y" && chosenOption != "N" && chosenOption != "n")
+            {
+                Console.WriteLine("File already exists!\nDo you want to override? " +
+                                  "(\"Y\"\\\"N\") (Enter \"Q\" to quit)");
+                chosenOption = Console.ReadLine();
+                if (chosenOption == "Q" || chosenOption == "q") Environment.Exit(0);
+            }
+            if (chosenOption == "Y" || chosenOption == "y") return true;
+
+            return false;
+        }
+
+        private static void WriteTextToFile(string fileName, string result)
+        {
             File.WriteAllText(fileName, result, System.Text.Encoding.UTF8);
 
             Console.ForegroundColor = ConsoleColor.Green;
